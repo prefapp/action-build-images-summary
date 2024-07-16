75aa3784171074f317ce2c7fd79816bc201777e5
tonirvega <arvegadev@hotmail.com>
2024-07-16 11:14:36 +0200

    wip

--- dist/index.js
+++ dist/index.js
@@ -29559,7 +29559,7 @@ function getContext() {
     getCoreInputs()
 
   // Init the github context and the octokit client
-  const { owner, repo } = github.repo
+  const { owner, repo } = github.context
 
   const octokit = github.getOctokit(token)
 
